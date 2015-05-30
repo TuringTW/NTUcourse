148cb74c8b3027515a635a58d66a54701120f3ed
TuringTW <kevin831130810620@yahoo.com.tw>
2015-05-31 03:03:28 +0800

    Revert "add play.py for audio test"
    This reverts commit 95300b0111ce7c4ab1abde69d73c91be8d822b80.

--- 2015_spring_electronics_exp_hearing_aid/wire/go.py
+++ 2015_spring_electronics_exp_hearing_aid/wire/go.py
@@ -1,6 +1,5 @@
 import pyaudio
 import time
-import numpy
 
 WIDTH = 2
 CHANNELS = 2
@@ -9,8 +8,7 @@ RATE = 44100
 p = pyaudio.PyAudio()
 
 def callback(in_data, frame_count, time_info, status):
-	# print(in_data);
-	return (in_data, pyaudio.paContinue)
+    return (in_data, pyaudio.paContinue)
 
 stream = p.open(format=p.get_format_from_width(WIDTH),
                 channels=CHANNELS,
